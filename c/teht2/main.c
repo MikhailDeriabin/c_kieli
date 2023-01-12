d7a62eb6ca45db1d23866f7d39199e694ee81619
Mikhail <mikko.deriabin@gmail.com>
2023-01-12 19:02:44 +0200

    change teht2

--- c/teht2/main.c
+++ c/teht2/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#define maxInputSize 50
 
 int main(){
-    char userInput[50];
+    char userInput[maxInputSize];
 
     printf("Enter a string: ");
     scanf("%50s", &userInput);
@@ -9,12 +10,15 @@ int main(){
     char* pUserInput = userInput;
     while(*pUserInput != '\0')
         pUserInput++;
-    
 
-    while(pUserInput >= userInput){
-        printf("%c", *pUserInput);
+    char reverseInput[maxInputSize];
+    for(int i=0; pUserInput>=userInput; i++){
         pUserInput--;
+        reverseInput[i] = *pUserInput;
     }
+    reverseInput[maxInputSize-1] = '\0';
+
+    printf("%s", reverseInput);
 
     return 0;
 }
