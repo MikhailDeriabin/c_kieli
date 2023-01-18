e01766731cf680ba603195e7aebf9b0e252a4ed0
Mikhail <mikko.deriabin@gmail.com>
2023-01-18 18:08:04 +0200

    add teht4

--- c/teht4/main.c
+++ c/teht4/main.c
@@ -7,18 +7,41 @@ osoitinta
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
 
-#define numsSize 10
-
-void zeroIntArr(int* arr, int size);
-void printIntArr(int* arr, int size);
-void askIntNum(int* result, char* msg, int min, int max);
+#define numsSize 5
 
 int main(){
     int nums[numsSize];
+    int* pNums = nums;
+    pNums++;
     zeroIntArr(nums, numsSize);
 
-    
+    int maxInputNumCount = floor(numsSize/2);
+    char maxInputNumCountStr[5];
+    intToStr(maxInputNumCount, maxInputNumCountStr);
+    int inputedNumCount = 1;
+
+    while (inputedNumCount <= maxInputNumCount){
+        int input;
+
+        char inputedNumCountStr[5];
+        intToStr(inputedNumCount, inputedNumCountStr);
+        char* messages[] = { inputedNumCountStr, "/",  maxInputNumCountStr, " num: "};
+        char msg[20] = "\0";
+        concatStings(msg, messages, 4);
+
+        askIntNum(&input, msg, 1, 1000);
+        *pNums = input;
+        pNums+=2;
+        inputedNumCount++;
+    }
+    pNums = nums;
+
+    printIntArr(nums, numsSize);
 
     return 0;
 }
@@ -32,8 +55,7 @@ void zeroIntArr(int* arr, int size){
 
 void printIntArr(int* arr, int size){
     for(int i=0; i<size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+        printf("Elem %d has value %d and RAM address %p\n", i+1, arr[i], &arr[i]);     
 }
 
 void askIntNum(int* result, char* msg, int min, int max){
@@ -46,5 +68,15 @@ void askIntNum(int* result, char* msg, int min, int max){
     } else{
         printf("Wrong value, please try again\n");
         askIntNum(result, msg, min, max);
+    }
+}
+
+void intToStr(int num, char* str){
+    snprintf(str, sizeof(str), "%d\0", num);
+}
+
+void concatStings(char* result, char** strings, int stringsSize){
+    for(int i=0; i<stringsSize; i++){
+        strcat(result, strings[i]);
     }
 }
